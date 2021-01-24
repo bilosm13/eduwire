20c3f4a525b192e0e840b524207d6f4ede5d78d6
jessmoody <jmoody@student.mtsac.edu>
2021-01-24 10:00:33 -0800

    added firebase code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
+import firebase from "firebase";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPcqOzpPh2h_woDmw52Hbr0QGxODs4BlQ",
+  authDomain: "qwerhacks2021-project.firebaseapp.com",
+  projectId: "qwerhacks2021-project",
+  storageBucket: "qwerhacks2021-project.appspot.com",
+  messagingSenderId: "333611052365",
+  appId: "1:333611052365:web:5b586a6bad3e02b99adb04",
+  measurementId: "G-LV5RT899J1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
 
 ReactDOM.render(
   <React.StrictMode>
