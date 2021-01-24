e2c38dcefd7225b9b2624b1919698ed542e903be
Stephanie Bilodeau <sbilodeau@gatech.edu>
2021-01-24 01:57:17 -0500

    major update to app structure for web form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+//import FileUploader from './components/FileUploader';
+import './App.css'; //centers form and other content on page
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const submitForm = () =>{
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("file", selectedFile);
+
+    //axios //this is for uploading your information to a specific address/server/website using axios
+      //.post(UPLOAD_URL, formData)
+      //.then((res) => {
+        //alert("File Upload Success");
+     // })
+      //.catch((err) => alert("File Upload Error"));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input 
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)} 
+        />
+
+        <input
+          type="file"
+          value={selectedFile}
+          onChange={(e) => setSelectedFile(e.target.files[0])}
+        //FileUploader //this is a separate component that I can't get to work
+          //onFileSelectSuccess={(file) => setSelectedFile(file)}
+          //onFileSelectError={({ error }) => alert(error)}
+        />
+
+        <button onClick={submitForm}>Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/MultiUpload.js
@@ -0,0 +1,22 @@
+class ImageUpload extends React.Component {
+  state = {
+    files: []
+  }
+
+  fileSelectedHandler = (e) => {
+    this.setState({ files: [...this.state.files, ...e.target.files] })
+  }
+
+  render() {
+    return (
+      <form>
+        <div><h2>Upload images</h2></div>
+        <h3>Images</h3>
+        <input type="file" multiple onChange={this.fileSelectedHandler} />
+      </form>
+    )
+  }
+}
+
+
+ReactDOM.render(<ImageUpload />, document.getElementById('app'))
